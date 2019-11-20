8139695c9a35fbe453919269942c9046a81e0cf8
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-20 17:21:43 +0530

    ObjC - Base Project Added

---
+++ Screenshot_ObjC/Screenshot_ObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Screenshot_ObjC
+//
+//  Created by mallikarjun on 20/11/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Screenshot_ObjC/Screenshot_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Screenshot_ObjC
+//
+//  Created by mallikarjun on 20/11/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
