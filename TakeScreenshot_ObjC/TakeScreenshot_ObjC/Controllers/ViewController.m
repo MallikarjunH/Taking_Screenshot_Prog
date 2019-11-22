3eb0ed948875b350a7c79c6d81fe4c561121c61d
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-22 17:31:40 +0530

    Swift Project - Update
    * VC added
    * Permission added

--- TakeScreenshot_ObjC/TakeScreenshot_ObjC/Controllers/ViewController.m
+++ TakeScreenshot_ObjC/TakeScreenshot_ObjC/Controllers/ViewController.m
@@ -19,5 +19,41 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)takeScreenhot:(id)sender {
+    
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
+        UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, NO, [UIScreen mainScreen].scale);
+    } else {
+        UIGraphicsBeginImageContext(self.view.bounds.size);
+    }
+    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    NSData *data = UIImagePNGRepresentation(image);
+    [data writeToFile:@"snapshot.png" options:NSDataWritingWithoutOverwriting error:Nil];
+    [data writeToFile:@"snapshot.png" atomically:YES];
+
+    UIImageWriteToSavedPhotosAlbum([UIImage imageWithData:data], nil, nil, nil);
+}
+
+- (UIImage*)captureView:(UIView *)view
+{
+    CGRect rect = [[UIScreen mainScreen] bounds];
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [view.layer renderInContext:context];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
+}
+
+- (void)saveScreenshotToPhotosAlbum:(UIView *)view
+{
+    UIImageWriteToSavedPhotosAlbum([self captureView:self.view], nil, nil,nil);
+}
+
+
 
 @end
---
+++ TakeScreenshot_Swift/TakeScreenshot_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeScreenshot_Swift
+//
+//  Created by mallikarjun on 21/11/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
